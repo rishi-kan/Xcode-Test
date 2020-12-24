28e012013bcfe3ead91850a83f67f4f80ced0a45
rishi-kan <73443819+rishi-kan@users.noreply.github.com>
2020-12-24 12:51:14 -0500

    Add files via upload

---
+++ yes/yesApp.swift
@@ -0,0 +1,17 @@
+//
+//  yesApp.swift
+//  yes
+//
+//  Created by Rishi Kandagadla on 12/24/20.
+//
+
+import SwiftUI
+
+@main
+struct yesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
